f00167c8acdd5862a404deafde4086973150e340
Alfredo Parra <luisalfredoparrajorge@gmail.com>
2022-09-06 17:16:54 -0500

    Se modifica el archivo agregar_usuario para llamar la funcion respectiva

--- model/agregar_usuario.php
+++ model/agregar_usuario.php
@@ -3,15 +3,11 @@
 if (!empty($_POST)) {
   if (isset($_POST["documento"]) && isset($_POST["ndocumento"]) && isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["fecha_nacimiento"]) && isset($_POST["lugar_nacimiento"]) && isset($_POST["fecha_documento"]) && isset($_POST["lugar_documento"]) && isset($_POST["mail"]) && isset($_POST["telefono"]) && isset($_POST["direccion"])) {
     if ($_POST["documento"] != "" && $_POST["ndocumento"] != "" && $_POST["nombre"] != "" && $_POST["apellido"] != "" && $_POST["fecha_nacimiento"] != "" && $_POST["lugar_nacimiento"] != "" && $_POST["fecha_documento"] != "" && $_POST["lugar_documento"] != "" && $_POST["mail"] != "" && $_POST["telefono"] != "" && $_POST["direccion"] != "") {
-      include "conexion.php";
+      include_once "usuario.php";
 
-      $sql = "INSERT INTO usuario(Tipo_Documento,Número de Documento,Nombres Completos,Apellidos,Sexo,Teléfono_Celular, Teléfono_Fijo, Correo_Electrónico, Municipio, Dirección, Barrio-Vereda, Fecha_Nacimiento, Etnia, Condición_Discapacidad, Estrato_Residencia, U_Nivel_Educativo, Acceso_Dispositivos_E, Dispositivos_Tecnológicos, Conectividad_Internet) value (\"$_POST[documento]\",\"$_POST[ndocumento]\",\"$_POST[nombre]\",\"$_POST[apellido]\",\"$_POST[fecha_nacimiento]\",\"$_POST[documento]\",\"$_POST[ndocumento]\",\"$_POST[nombre]\",\"$_POST[apellido]\",\"$_POST[fecha_nacimiento]\",\"$_POST[documento]\",\"$_POST[ndocumento]\",\"$_POST[nombre]\",\"$_POST[apellido]\",\"$_POST[fecha_nacimiento]\"";
-      $query = $con->query($sql);
-      if ($query != null) {
-        print "<script>alert(\"Agregado exitosamente.\");window.location='../ver.php';</script>";
-      } else {
-        print "<script>alert(\"No se pudo agregar.\");window.location='../ver.php';</script>";
-      }
-    }
+      $usuario = new usuario();
+
+      $usuario -> agregar_usuario("$_POST[documento]","$_POST[ndocumento]","$_POST[nombre]","$_POST[apellido]","$_POST[sexo]","$_POST[celular]","$_POST[fijo]","$_POST[mail]","$_POST[municipio]","$_POST[direccion]","$_POST[barrio]","$_POST[fecha_nacimiento]","$_POST[Etnia]","$_POST[discapacidad]","$_POST[estrato]", "$_POST[nivel_educativo]","$_POST[p_dispositivos]","$_POST[dispositivos]","$_POST[conectividad]");
   }
+}
 }
