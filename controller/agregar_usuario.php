fcfa03af2bd3184b61f4184ad98183a666e00316
Alfredo Parra <luisalfredoparrajorge@gmail.com>
2022-09-06 18:21:06 -0500

    Se reestructura el esquema del sistema y se empieza a crear el sondeo

--- controller/agregar_usuario.php
+++ controller/agregar_usuario.php
@@ -4,10 +4,10 @@
 
 if (!empty($_POST)) {
   if (isset($_POST["documento"]) && isset($_POST["ndocumento"]) && isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["sexo"]) && isset($_POST["celular"]) && isset($_POST["fijo"]) && isset($_POST["mail"]) && isset($_POST["municipio"]) && isset($_POST["direccion"]) && isset($_POST["barrio"])) {
-    print_r("Segundo if");
+
     if ($_POST["documento"] != "" && $_POST["ndocumento"] != "" && $_POST["nombre"] != "" && $_POST["apellido"] != "" && $_POST["sexo"] != "" && $_POST["celular"] != "" && $_POST["fijo"] != "" && $_POST["mail"] != "" && $_POST["municipio"] != "" && $_POST["direccion"] != "" && $_POST["barrio"] != "") {
 
-      include_once "usuario.php";
+      include_once "../model/usuario.php";
       $usuario = new usuario();
 
       $usuario->agregar_usuario("$_POST[documento]", "$_POST[ndocumento]", "$_POST[nombre]", "$_POST[apellido]", "$_POST[sexo]", "$_POST[celular]", "$_POST[fijo]", "$_POST[mail]", "$_POST[municipio]", "$_POST[direccion]", "$_POST[barrio]", "$_POST[fecha_nacimiento]", "$_POST[Etnia]", "$_POST[discapacidad]", "$_POST[estrato]", "$_POST[nivel_educativo]", "$_POST[p_dispositivos]", "$_POST[dispositivos]", "$_POST[conectividad]");
---
+++ model/sondeo.php
@@ -0,0 +1,21 @@
+<?php
+
+include "conexión.php";
+
+class sondeo
+{
+
+  public function agregar_sondeo($tp, $nd, $nc, $a, $s, $tc, $tf, $ce, $m, $dir, $bv, $fn, $e, $cd, $er, $unv, $ade, $de, $ci)
+  {
+    $proceso = $GLOBALS['bd']->prepare("INSERT INTO `usuario` (`Tipo_Documento`, `Número de Documento`, `Nombres Completos`, `Apellidos`, `Sexo`, `Teléfono_Celular`, `Teléfono_Fijo`, `Correo_Electrónico`, `Municipio`, `Dirección`, `Barrio-Vereda`, `Fecha_Nacimiento`, `Etnia`, `Condición_Discapacidad`, `Estrato_Residencia`, `U_Nivel_Educativo`, `Acceso_Dispositivos_E`, `Dispositivos_Tecnológicos`, `Conectividad_Internet`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    $resultado = $proceso->execute([$tp, $nd, $nc, $a, $s, $tc, $tf, $ce, $m, $dir, $bv, $fn, $e, $cd, $er, $unv, $ade, $de, $ci]);
+
+    if ($resultado === TRUE) {
+      header("Location: ../vista/inicioU.html");
+      exit();
+    } else {
+      header("Location: ../vista/resgistrar_usuario.html?mensaje=error");
+      exit();
+    }
+  }
+}
